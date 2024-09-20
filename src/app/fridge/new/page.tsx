ff1105c0259f90de362d7a74eab508faf42a060a
tamo-tmry <mollianium@gmail.com>
2024-09-20 22:59:55 +0900

    feat: UI調整

--- src/app/fridge/new/page.tsx
+++ src/app/fridge/new/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function FridgeNew() {
+  const router = useRouter();
   const [name, setName] = useState("");
   const [expirationDate, setExpirationDate] = useState("");
 
   const addFridgeItem = () => {
+    if (!name || !expirationDate) return;
     const fridgeItemsJSON = localStorage.getItem("fridgeItems");
     const originFridgeItems = fridgeItemsJSON
       ? JSON.parse(fridgeItemsJSON)
       : [];
     const item = [...originFridgeItems, { name, expirationDate }];
     localStorage.setItem("fridgeItems", JSON.stringify(item));
+    router.push("/fridge");
   };
 
   return (
@@ -21,15 +25,19 @@ export default function FridgeNew() {
         name="name"
         type="text"
         value={name}
+        className="bg-black border border-white"
         onChange={(e) => setName(e.target.value)}
       />
       <input
         name="expirationDate"
         type="date"
         value={expirationDate}
+        className="mt-4 bg-black border border-white"
         onChange={(e) => setExpirationDate(e.target.value)}
       />
-      <button onClick={() => addFridgeItem()}>冷蔵庫に入れる</button>
+      <button className="mt-4" onClick={() => addFridgeItem()}>
+        冷蔵庫に入れる
+      </button>
     </form>
   );
 }
--- src/app/fridge/page.tsx
+++ src/app/fridge/page.tsx
@@ -2,10 +2,16 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import FridgeList from "@/app/components/fridge/List";
+import Link from "next/link";
 
 export default async function Fridge() {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/login");
 
-  return <FridgeList />;
+  return (
+    <>
+      <FridgeList />
+      <Link href="/fridge/new">冷蔵庫に入れる</Link>
+    </>
+  );
 }
