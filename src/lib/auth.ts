01798d95a2bba86d23247d63fea6dc17579dcbfe
tamo-tmry <mollianium@gmail.com>
2024-09-20 23:48:57 +0900

    chore: add secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,17 @@
-import {NextAuthOptions} from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { getServerSession as originalGetServerSession } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID || "",
-            clientSecret: process.env.GOOGLE_SECRET || ""
-        })
-    ],
-}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 export const getServerSession = async () => {
-    return originalGetServerSession(authOptions);
+  return originalGetServerSession(authOptions);
 };
