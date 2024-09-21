d04656a4160ea4f6fdcad601375340cc6993a4cf
tamo-tmry <mollianium@gmail.com>
2024-09-21 12:14:05 +0900

    chore: add debug code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+console.log("DEBUG env: ", process.env);
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
